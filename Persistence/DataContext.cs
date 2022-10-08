468651c71e73dec7858f4048447d44f4c0dd8baa
Lint404 <klattkl@gmatc.matc.edu>
2022-10-08 16:51:31 -0500

    Created seed class.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Persistence
     {
         public DbSet<WeatherForecast> WeatherForecasts {get; set;}
 
+        public DbSet <Post> Posts { get; set; }
         public string DbPath {get;}
 
         public DataContext(){
