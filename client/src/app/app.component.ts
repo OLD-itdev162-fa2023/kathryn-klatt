046b8d86687e6257c0a1a876229ea65293ed9e9a
Lint404 <klattkl@gmatc.matc.edu>
2022-11-15 19:04:38 -0600

    Added home, view-post, create post components.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'client';
-  posts:any;
-
-  constructor(private http: HttpClient){}
-
-  ngOnInit(): void {
-      this.http.get('http://localhost:5010/api/posts').subscribe(
-        response => { this.posts = response; },
-        error => {console.log(error);}
-      );
-  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { HomeComponent } from './home/home.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponent,
+    HomeComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
